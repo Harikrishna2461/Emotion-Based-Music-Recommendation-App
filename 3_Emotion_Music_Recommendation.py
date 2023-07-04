813e4a4f8ec3520c0ee744257d71f908329cc4f4
Hari Krishna Dhamodaran <67218513+Harikrishna2461@users.noreply.github.com>
2023-07-04 19:03:07 +0530

    Add files via upload

---
+++ 3_Emotion_Music_Recommendation.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import numpy as np
+from keras.models import load_model
+from PIL import Image,ImageOps
+import webbrowser
+
+st.header("Emotion Based Music Recommendation System")
+st.set_option("deprecation.showfileUploaderEncoding",False)
+
+CNN_Model = load_model('3_Emotion_Recognition_CNN_Model.h5')
+Labels = ['Neutral','Happy','Sad']
+
+Language = st.text_input("Please Select your preferred Language(If No Preference Enter None)") #Collecting the Language Prefernce of the User as Input.
+Singer = st.text_input("Please Select your Favourite Singer(If No Preference Enter None)") #Collecting the Language Prefernce of the User as Input.
+
+st.write("""
+         # Emotion Based Music Recommendation
+				 """)
+
+User_Image = st.file_uploader("Please Choose an Image",type=["jpg","png"])
+
+def load_image_and_predict(image_data,Model):
+  size = (96,96)
+  #image = ImageOps.fit(image_data,size,Image.ANTIALIAS)
+  img = image_data.resize(size)
+  img = np.asarray(img)
+  img_reshape = img[np.newaxis,...]
+  prediction = Model.predict(img_reshape)
+  emotion = np.argmax(prediction)
+  return emotion
+
+button = st.button("Recommend me some songs according to my mood")
+
+try:
+	Uploaded_Image = Image.open(User_Image)
+	st.image(Uploaded_Image,use_column_width=True)
+	val = load_image_and_predict(Uploaded_Image,CNN_Model)
+	Emotion = Labels[val]
+
+	if button:
+		link = ""
+		if not(Emotion):
+			st.warning("Please let me capture your emotion properly")
+		else:
+			if Language == "None" and Singer == "None":
+				link = "https://www.youtube.com/results?search_query=" + str(Emotion) + "songs"
+				st.write("Click the link to listen to the song [link](link)")
+			elif Language == "None":
+				link = "https://www.youtube.com/results?search_query=" + str(Singer) + str(Emotion) + "songs"
+				st.write("Click the link to listen to the song [link](link)")
+			elif Singer == "None":
+				link = "https://www.youtube.com/results?search_query=" + str(Language) + str(Emotion) + "songs"
+				st.write("Click the link to listen to the song [link](link)")
+			else:
+				link = "https://www.youtube.com/results?search_query=" + str(Language) + str(Singer) + str(Emotion) + "songs"
+				st.write("Click the link to listen to the song [link](link)")
+				
+
+except:
+	print("Please choose the image first!")
